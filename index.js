2372035a99397d5417e99e52013c9ac76845a34c
AgustD <agusdwisaputra159@gmail.com>
2021-04-19 05:46:14 +0700

    b

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const {
-    WAConnection
+    WAConnection,
+    MessageType
 } = require("@adiwajshing/baileys");
 const fs = require("fs");
+
+const prefix = "!"
 
 async function connectToWhatsApp() {
     const conn = new WAConnection();
@@ -9,6 +12,28 @@ async function connectToWhatsApp() {
     fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json');
     await conn.connect();
     fs.writeFileSync('./session.json', JSON.stringify(conn.base64EncodedAuthInfo(), null, '\t'));
+
+    conn.on('chat-update', async (chat) => {
+        try {
+            if (!chat.hasNewMessage) return;
+            chat = chat.messages.all()[0];
+            if (!chat.message) return;
+            if (chat.key && chat.key.remoteJid == 'status@broadcast') return;
+            if (chat.key.fromMe) return;
+            const from = chat.key.remoteJid;
+            const type = Object.keys(chat.message)[0];
+            body = (type === 'conversation' && chat.message.conversation.startsWith(prefix)) ? chat.message.conversation : (type == 'imageMessage') && chat.message.imageMessage.caption.startsWith(prefix) ? chat.message.imageMessage.caption : (type == 'videoMessage') && chat.message.videoMessage.caption.startsWith(prefix) ? chat.message.videoMessage.caption : (type == 'extendedTextMessage') && chat.message.extendedTextMessage.text.startsWith(prefix) ? chat.message.extendedTextMessage.text : ''
+            const command = body.split(/ +/).shift().toLowerCase()
+
+            switch (command) {
+                case prefix + "tes":
+                    conn.sendMessage(from, "OK", MessageType.text);
+                    break;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
 }
 // run in main file
 connectToWhatsApp()
